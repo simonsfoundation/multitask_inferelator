ee37f33b46b0a7b49844dc09ca3d9039db0e00f7
Dayanne Castro <dayannecastro@Dayannes-MacBook-Pro.local>
2018-03-18 11:36:52 -0400

    joblib with threading and verbose

--- inferelator_ng/amusr.py
+++ inferelator_ng/amusr.py
@@ -207,7 +207,7 @@ class AmuSR_regression:
         '''
 
         # targets = ['BSU02100', 'BSU05340', 'BSU24010', 'BSU24040'] # test
-        results = Parallel(n_jobs=n_jobs)(delayed(run_regression_EBIC)(design, response,
+        results = Parallel(n_jobs=n_jobs, backend='threading', verbose=np.ceil(len(targets)/10.))(delayed(run_regression_EBIC)(design, response,
                                     regulators, target, priors, prior_weight) for target in targets)
 
         weights = []
